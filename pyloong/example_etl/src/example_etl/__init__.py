6147c2af36726d1cc61c1781aa9e0af9b5631f9d
chenlong <longchen2008@126.com>
2024-06-24 11:44:49 +0800

    [del]刪除項目

--- pyloong/example_etl/src/example_etl/__init__.py
+++
@@ -1,2 +0,0 @@
-"""example-etl"""
-__version__ = '0.1.0'
--- pyloong/example_etl/tests/test_version.py
+++
@@ -1,7 +0,0 @@
-"""Tests"""
-from example_etl import __version__
-
-
-def test_version():
-    """Test version"""
-    assert __version__ == '0.1.0'
